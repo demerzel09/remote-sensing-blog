4fe1b858670e0368cf9f3bf35a8c09b6eb256ce6
demerzel09 <44060600+demerzel09@users.noreply.github.com>
2025-06-09 21:05:45 +0900

    Sanitize sentinel credentials

--- scripts/setenv_dataspace_copernicus_eu.sh
+++ scripts/setenv_dataspace_copernicus_eu.sh
@@ -1,2 +1,4 @@
-export SENTINEL_USER=okubo@fusic.co.jp
-export SENTINEL_PASSWORD=am~,w,JL8YfjqTE
+# Placeholder credentials for the Copernicus Data Space API.
+# Replace with your own values and **do not commit real credentials**.
+export SENTINEL_USER=<your username>
+export SENTINEL_PASSWORD=<your password>
