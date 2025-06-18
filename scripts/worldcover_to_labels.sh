898f902b91e02146a5139d2fd23fb5f0539802d4
demerzel09 <albatross.jp@gmail.com>
2025-06-18 14:31:07 +0900

    引数を実際にあわせる

--- scripts/worldcover_to_labels.sh
+++ scripts/worldcover_to_labels.sh
@@ -8,8 +8,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Example paths. WORLD_COVER will be downloaded if it does not already exist.
 
 WORLD_COVER="data/worldcover/ESA_WorldCover_10m_2021_v100_Map.tif"
-REFERENCE="data/raw/B02.tif"
-OUTPUT="data/raw/labels.tif"
+REFERENCE="data/example_run/Sentinel-2/35.6000_139.7000_2024-01-01_2024-01-31/B02.tif"
+OUTPUT="data/example_run/Sentinel-2/35.6000_139.7000_2024-01-01_2024-01-31/labels.tif"
 
 python -m src.utils.worldcover_to_labels \
     --worldcover "$WORLD_COVER" \
