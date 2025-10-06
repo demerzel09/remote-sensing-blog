612dd6ec2aa06bede255e623865adaecc82a0ed2
demerzel09 <44060600+demerzel09@users.noreply.github.com>
2025-10-06 19:37:58 +0900

    Highlight mismatched pixels in difference raster

--- src/utils/io_raster.py
+++ src/utils/io_raster.py
@@ -1,7 +1,17 @@
+from typing import Mapping, Optional, Tuple
+
 import rasterio
 
 
-def write_raster(path, array, meta):
+def write_raster(
+    path,
+    array,
+    meta,
+    colormap: Optional[Mapping[int, Mapping[int, Tuple[int, int, int, int]]]] = None,
+):
     """Write a raster array to disk."""
     with rasterio.open(path, 'w', **meta) as dst:
         dst.write(array)
+        if colormap:
+            for band, cmap in colormap.items():
+                dst.write_colormap(band, dict(cmap))
